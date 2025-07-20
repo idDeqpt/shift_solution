82e2153da455fe2f238b4131a088077222b20a0d
Владислав Ушаков <ushakov_vl@list.ru>
2025-07-20 23:03:50 +0700

    Added errors checking

---
+++ ExecuteErrors.bat
@@ -0,0 +1,7 @@
+java -jar util.jar -s -a -p
+java -jar util.jar -s -a -o
+java -jar util.jar -s -p -e sample- in1.txt in2.txt bebe.txt
+java -jar util.jar -f -a -p sample-
+java -jar util.jar -f -a -p sample- -o files in1
+
+pause
